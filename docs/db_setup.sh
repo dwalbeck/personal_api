6176dc5a670521561412cde6e96287f678c3d1d0
Davey Walbeck <daveywalbeck@gmail.com>
2026-01-01 10:53:14 -0700

    added github action

---
+++ docs/db_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
+  CREATE ROLE apiuser WITH LOGIN PASSWORD '@p!u53Rt0k3n';
+  CREATE GROUP application LOGIN;
+  CREATE GROUP dba SUPERUSER CREATEDB CREATEROLE LOGIN;
+  GRANT application TO apiuser;
+
+  CREATE DATABASE personal-ai WITH OWNER = apiuser;
+
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE EXTENSION IF NOT EXISTS vector;
+
+  CREATE TABLE IF NOT EXISTS portfolio_embeddings (
+    id SERIAL PRIMARY KEY,
+    content TEXT NOT NULL,
+    embedding VECTOR(1536) NOT NULL
+  );
+
+  GRANT ALL PRIVILEGES ON DATABASE "personal_ai" TO apiuser;
+
+EOSQL
